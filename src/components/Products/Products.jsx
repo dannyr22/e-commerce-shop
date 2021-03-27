58054546adc9e004f67050420fe9f6697063febc
dannyr22 <dannyr2763@hotmail.com>
2021-03-27 16:51:37 +0000

    product styles added using material UI

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,24 @@
-import Grid from '@material-ui/core/grid';
+import {Grid} from '@material-ui/core';
+import Product from './Product/Product';
 
 const products = [
-  { id: 1, name: 'Shoes', description: 'running shoes'}
-  { id: 2, name: 'Macbook', description: 'apple macbook'}
+  { id: 1, name: 'Shoes', description: 'running shoes', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg'},
+  { id: 2, name: 'Macbook', description: 'apple macbook', price: '$10', image: 'https://cdn.thewirecutter.com/wp-content/media/2020/12/macbook-2048px-9.jpg?auto=webp&quality=60&crop=1.91:1&width=1200'}
 ]
 
 const Products = () => {
-  <main>
+  return (
+    <main>
     <Grid container justify="center" spacing={4}>
       {products.map(product => (
         <Grid item key={product.id} sx={12} sm={6} m={4} lg={3}>
-          <Product />
+          <Product product={product} />
         </Grid>
       ))}
     </Grid>
   </main>
+  )
+
 }
+
+export default Products
