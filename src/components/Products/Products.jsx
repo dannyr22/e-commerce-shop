40ebf8324763782e65d08cca22ac077650ee6fe2
dannyr22 <dannyr2763@hotmail.com>
2021-03-29 20:29:48 +0100

    commerce styled

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'Shoes', description: 'running shoes', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg'},
@@ -7,8 +8,10 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles() 
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
     <Grid container justify="center" spacing={4}>
       {products.map(product => (
         <Grid item key={product.id} sx={12} sm={6} m={4} lg={3}>
