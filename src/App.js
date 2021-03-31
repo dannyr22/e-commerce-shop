2cf912b49992603f36b4b82b9d9f2c7988f25ef4
dannyr22 <dannyr2763@hotmail.com>
2021-03-31 12:53:36 +0100

    cart onClick event setup

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import Products from './components/Products/Products'
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
 
   const fetchProducts = async () => {
     const {data} = await commerce.products.list();
     setProducts(data)
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity)
+    setCart(item.cart)
+  }
   useEffect(() => {
     fetchProducts()
+    fetchCart()
   }, [])
 
-  console.log(products)
+  console.log(cart)
   return (
     <div>
       <Navbar />
-       <Products />
+       <Products products={products} onAddToCart={handleAddToCart}/>
     </div>
   )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,12 +2,8 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  { id: 1, name: 'Shoes', description: 'running shoes', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg'},
-  { id: 2, name: 'Macbook', description: 'apple macbook', price: '$10', image: 'https://cdn.thewirecutter.com/wp-content/media/2020/12/macbook-2048px-9.jpg?auto=webp&quality=60&crop=1.91:1&width=1200'}
-]
 
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
   const classes = useStyles() 
   return (
     <main className={classes.content}>
@@ -15,7 +11,7 @@ const Products = () => {
     <Grid container justify="center" spacing={4}>
       {products.map(product => (
         <Grid item key={product.id} sx={12} sm={6} m={4} lg={3}>
-          <Product product={product} />
+          <Product product={product} onAddToCart={onAddToCart}/>
         </Grid>
       ))}
     </Grid>
